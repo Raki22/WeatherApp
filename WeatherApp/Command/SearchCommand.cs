ea5ef8687623e0e189046a8c3dc98bdfd654d2a5
Raki22 <lachraf.raki@gmail.com>
2020-10-28 21:10:35 +0100

    initial commit

---
+++ WeatherApp/Command/SearchCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WeatherApp.ViewModel;
+using WeatherApp.ViewModel.Helpers;
+
+namespace WeatherApp.Command
+{
+    public class SearchCommand : ICommand
+    {
+        public MainViewModel MainViewModel { get; set; }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public SearchCommand(MainViewModel vm)
+        {
+            this.MainViewModel = vm;
+        }
+        public bool CanExecute(object parameter)
+        {
+            string query = parameter as string;
+
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            MainViewModel.MakeQuery();
+        }
+    }
+}
---
+++ WeatherApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Command;
+using WeatherApp.Model;
+using WeatherApp.ViewModel.Helpers;
+
+namespace WeatherApp.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<City> Cities { get; set; }
+        private string _queryTextBox;
+        public string QueryTextBox
+        {
+            get { return _queryTextBox; }
+            set 
+            { 
+                _queryTextBox = value;
+                OnPropertyChanged("QueryTextBox");
+            }
+        }
+
+        private CurrentConditions _currentConditions;
+        public CurrentConditions CurrentConditions
+        {
+            get { return _currentConditions; }
+            set 
+            { 
+                _currentConditions = value;
+                OnPropertyChanged("CurrentConditions");
+            }
+        }
+
+        private City _selectedCity;
+        public City SelectedCity
+        {
+            get { return _selectedCity; }
+            set 
+            { 
+                _selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+                GetCurrentConditions();
+            }
+        }
+
+        public SearchCommand SearchCommand { get; set; }
+
+        public MainViewModel()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                SelectedCity = new City() { LocalizedName = "El Oued" };
+                CurrentConditions = new CurrentConditions()
+                {
+                    WeatherText = "Partly Cloudy",
+                    Temperature = new Temperature() { Metric = new Measure() { Value = "21" } }
+                };
+            }
+
+            SearchCommand = new SearchCommand(this);
+            Cities = new ObservableCollection<City>();
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            // by human language this means : if PropertyChanged event does exist, then use its Invoke method...
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async void MakeQuery()
+        {
+            var cities = await AccuWeatherHelper.GetCities(QueryTextBox);
+            
+            Cities.Clear();
+            foreach (var city in cities)
+            {
+                Cities.Add(city);
+            }
+        }
+
+        private async void GetCurrentConditions()
+        {
+            QueryTextBox = string.Empty;
+            Cities.Clear();
+            CurrentConditions = await AccuWeatherHelper.GetCurrentConditions(SelectedCity.Key);
+        }
+    }
+}
