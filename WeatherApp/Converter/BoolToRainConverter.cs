96608f85ddcfbd4c470529ab71e6d4c8adcdea09
Raki22 <lachraf.raki@gmail.com>
2020-10-29 12:05:49 +0100

    changing api key

---
+++ WeatherApp/Converter/BoolToRainConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WeatherApp.Converter
+{
+    public class BoolToRainConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isRaining = (bool)value;
+
+            if (isRaining)
+                return "raining ...";
+
+            return "not raining ...";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string rainStatus = (string)value;
+
+            if (rainStatus == "raining ...")
+                return true;
+
+            return false;
+        }
+    }
+}
